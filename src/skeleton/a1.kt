5c469f6d6c35373f25e09ba4e720b349f0caf1b1
Markus Larsson <markus.larsson@ddnd.se>
2019-10-30 13:51:57 +0100

    added simple skeleton

---
+++ src/skeleton/a1.kt
@@ -0,0 +1,23 @@
+package skeleton
+
+private fun readLn() = readLine()!! // string line
+private fun readInt() = readLn().toInt() // single int
+private fun readLong() = readLn().toLong() // single long
+private fun readFloat() = readLn().toFloat() // single float
+
+// Read line split by space
+private fun readStrings() = readLn().split(" ") // list of strings
+private fun readInts() = readStrings().map { it.toInt() } // list of ints
+private fun readLongs() = readStrings().map { it.toLong() } // list of longs
+private fun readFloats() = readStrings().map { it.toFloat() } // list of floats
+
+// val (n, k) = readInts() - destructuring declaration
+// println(a.joinToString("\n"))
+
+fun main() {
+    printOutput()
+}
+
+fun printOutput() {
+    // println(list.joinToString("\n")) - print output for list of strings
+}
