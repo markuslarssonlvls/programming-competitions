4364a61bf5358b0d4163e34a18846d7b9d02b51b
Markus Larsson <markus.larsson@ddnd.se>
2019-12-02 12:26:00 +0100

    problem 1

---
+++ src/adventofcode/ProblemOne.kt
@@ -0,0 +1,25 @@
+package adventofcode
+
+import kotlin.math.floor
+
+fun main() {
+    println(input1a.split("\n").map { it.toLong() }.map { floor(it.toDouble() / 3f) - 2 }.sum())
+
+    println(input1a.split("\n").map { it.toLong() }.map {
+        var sum = 0L
+        var fuel = it
+        while (fuel > 0) {
+            fuel = fuel.calculateFuel()
+            if (fuel > 0) {
+                sum += fuel
+            }
+        }
+        sum
+    }.sum())
+}
+
+fun Long.calculateFuel(): Long {
+    return (floor(this.toDouble() / 3f) - 2).toLong()
+}
+
+// 4882038
